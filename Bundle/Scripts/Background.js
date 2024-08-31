17c06b916672fb5a3b9f4c0a6fc493d53bdeee6f
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2024-08-31 17:07:10 -0400

    Sidebar stub

--- Bundle/Scripts/Background.js
+++ Bundle/Scripts/Background.js
@@ -1,2 +1,7 @@
 // Source/Background/mod.ts
 console.debug("Test");
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setPanelBehavior({
+    openPanelOnActionClick: true
+  });
+});
--- Source/Background/mod.ts
+++ Source/Background/mod.ts
@@ -1,2 +1,13 @@
 
+import { fetchUpdates } from './CheckUpdate'
+
 console.debug('Test')
+
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.sidePanel.setPanelBehavior({
+        openPanelOnActionClick : true
+    })
+})
+
+// fetchUpdates()
